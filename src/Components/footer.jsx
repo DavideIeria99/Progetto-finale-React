ffb5a932ab51a046a0158cc4eb74d3e0db682305
Davide Ieria <davideieria99@gmail.com>
2023-09-25 11:47:55 +0200

    footer

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -1,24 +1,70 @@
-import { Link } from "react-router-dom";
-import LanguageSwitcher from "./Switchers/LanguageSwitcher";
 
-export default function Footer() {
+'use client';
+
+import { Footer } from 'flowbite-react';
+import { BsFacebook, BsGithub, BsInstagram, BsTwitter } from 'react-icons/bs';
+
+export default function footer() {
   return (
-    <footer className="flex w-full justify-between border-t border-accent bg-gradient-to-r from-[#14496c] to-[#103954cc] p-4 text-white">
-      <ul className="flex items-center">
-        <Link className="mr-4 md:mr-12" to="/about-us">
-          About
-        </Link>
-        <Link className="mr-4 md:mr-12" to="/fake">
-          Privacy
-        </Link>
-        <Link className="mr-4 md:mr-12" to="/fake">
-          Contact
-        </Link>
-        <Link className="mr-4 md:mr-12" to="/fake">
-          Stack
-        </Link>
-      </ul>
-      <LanguageSwitcher />
-    </footer>
-  );
+
+    <Footer container bgDark='bg-[#283164] '>
+      <div className="w-full ">
+        <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
+          <div>
+            <h1 className="font-main  bg-[#283164] bg-gradient-to-r bg-clip-text pb-12 text-6xl font-extrabold text-transparent from-sky-600 to-sky-100">
+              {import.meta.env.VITE_PROJECT_NAME}
+            </h1>
+          </div>
+          <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="Follow us" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="https://github.com/DavideIeria99">
+                  Github
+                </Footer.Link>
+                <Footer.Link href="#">
+                  Discord
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">
+                  Privacy Policy
+                </Footer.Link>
+                <Footer.Link href="#">
+                  Terms & Conditions
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+            <Footer.Icon
+              href="#"
+              icon={BsFacebook}
+            />
+            <Footer.Icon
+              href="#"
+              icon={BsInstagram}
+            />
+            <Footer.Icon
+              href="#"
+              icon={BsTwitter}
+            />
+            <Footer.Icon
+              href="https://github.com/DavideIeria99"
+              icon={BsGithub}
+            />
+          </div>
+        </div>
+      </div>
+    </Footer >
+  )
 }
+
+
+
